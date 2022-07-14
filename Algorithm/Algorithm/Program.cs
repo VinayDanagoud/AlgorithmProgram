15eb226803b29444db3580c9d6ec95fe4c555dc7
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-07-14 17:56:11 +0530

    Binary Search

--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Algorithm Program");
+
+            BinarySearch binarySearch = new BinarySearch();
+            Console.WriteLine("Enter string value to search:");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("User input is :" + userInput);
+            binarySearch.BinarySearchIterative(userInput);
+        }
+    }
+}
